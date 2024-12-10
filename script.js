78fcda934a75c417aec8caba3da2a0800b4949bd
akash87925 <aaj317857@gmail.com>
2024-12-10 13:02:30 +0530

    addjsfile

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+    var navbar = document.getElementById("navbar");
+    if (navbar.className === "") {
+        navbar.className = "responsive";
+    } else {
+        navbar.className = "KISHKINDA UNIVERSITY";
+    }
+}
+
